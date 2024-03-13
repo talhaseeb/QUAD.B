f80f174a68295dd03c449695e0a2251df9aef14c
Rudra Patel <rudra.190410116095@gmail.com>
2024-03-13 01:24:37 -0400

    created_server and connected_mongo_atlas_url

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+const url = process.env.ATLAS_URI
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+mongoose.connect(url)
+    .then(() => {
+        console.log('MongoDB connected!')
+    }).catch((err) => {
+        console.log('MongoDB error: ' + err)
+    })
+
+app.get('/', (_, res) => {
+    res.json({
+        name: 'Server',
+        project: 'QUAD.B'
+    })
+})
+
+app.listen(PORT, console.log('Server running on PORT: ' + PORT))
