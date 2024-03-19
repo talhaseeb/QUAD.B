00292455161903ac56ff2d0da431f159accf4d29
MohdAbdulRahman7 <rahman77mohd@gmail.com>
2024-03-19 12:50:08 -0400

    added partner creation logic

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,38 @@
 const User = require('../models/user.model');
+const Partner = require('../models/partner.model');
+
+// Function to generate a new partnerId
+async function generatePartnerId() {
+    // Find the maximum partnerId in the collection
+    const maxPartner = await Partner.findOne().sort({ partnerId: -1 });
+    let nextPartnerId = 1;
+    if (maxPartner) {
+        nextPartnerId = maxPartner.partnerId + 1;
+    }
+    return nextPartnerId;
+}
 
 /* Controller function - CREATE (POST) a new user */
 exports.createUser = async (req, res) => {
+    // Assuming req.body contains user data
     try {
         const newUser = new User(req.body);
         const savedUser = await newUser.save();
+        
+        // Check if the user is a partner
+        if (req.body.isPartner) {
+            // Generate a new partnerId
+            const partnerId = await generatePartnerId();
+            
+            // Create a corresponding partner
+            const newPartner = new Partner({
+                partnerId: partnerId,
+                bannerImage: req.body.bannerImage,
+                userId: savedUser._id // Save the user's ObjectId as userId in the partner document
+            });
+            await newPartner.save();
+        }
+        
         res.status(201).json(savedUser);
     } catch (error) {
         res.status(500).json({ message: error.message });
