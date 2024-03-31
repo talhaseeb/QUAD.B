9f61d6a24ec73370433766894222e25bcfeb994c
Rudra Patel <rudra.190410116095@gmail.com>
2024-03-31 19:12:58 -0400

    deleted uwanted folders

--- vanilla/routes/notification.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const notificationController = require('../controllers/notification.controller');
-
-router.get('/:userId', notificationController.getNotifications);
-
-module.exports = router;
